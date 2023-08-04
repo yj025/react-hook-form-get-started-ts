3b1da91a7044fd8a00055fecb51a4957b2d22cd3
yj025 <108186517+yj025@users.noreply.github.com>
2023-08-04 15:09:45 +0800

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { useForm } from "react-hook-form";
+
+import "./styles.css";
+
+function App() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors }
+  } = useForm({
+    defaultValues: {
+      example: "",
+      exampleRequired: ""
+    }
+  });
+
+  console.log(watch("example")); // you can watch individual input by pass the name of the input
+
+  return (
+    <form
+      onSubmit={handleSubmit((data) => {
+        alert(JSON.stringify(data));
+      })}
+    >
+      <label>Example</label>
+      <input {...register("example")} defaultValue="test" />
+      <label>ExampleRequired</label>
+      <input
+        {...register("exampleRequired", { required: true, maxLength: 10 })}
+      />
+      {errors.exampleRequired && <p>This field is required</p>}
+      <input type="submit" />
+    </form>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
